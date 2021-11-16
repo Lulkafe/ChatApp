26e4b05f015aa807841f960cc9728505efc54130
Seimei Matsusaki <seimatsusaki@gmail.com>
2021-11-15 19:59:58 -0500

    Cleared unnecessary libs, files and lines

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,24 +4,13 @@ const http = require('http');
 const server = http.createServer(app);
 import generateRoomID from './roomIDGenerator';
 import { Request, Response } from 'express';
-import * as db from './db-driver';
+import * as db from './chatRoom';
 
-
-try {
-    (async () => { await db.connect(); })();
-    console.log('Connected to DB');
-} catch (e: unknown) {
-    if (e instanceof Error) {
-        console.error(e.message);
-        throw e;
-    }
-}
 
 app.get('/chat/new/roomID', (req: Request, res: Response) => {
     res.send('<h1>Hello</h1>');
     /*
       generate room ID(s)
-      ask the db if this is unique
     */
 });
 
